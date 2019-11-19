534ed405e280b0f2b6f42cd08df0474268318389
brenoos <breno.oliver07@gmail.com>
2019-11-18 21:11:55 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?key=0aaccf99";
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(),
+    ),
+  );
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
